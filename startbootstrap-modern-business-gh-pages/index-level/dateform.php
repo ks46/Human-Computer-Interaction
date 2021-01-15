4c36919a082f0ddb8035f41b09bbd7c16640fe11
Alex <alexgit0270@gmail.com>
2021-01-15 12:37:03 +0200

    PE to branch search working, moving to next segment

--- startbootstrap-modern-business-gh-pages/index-level/dateform.php
+++ startbootstrap-modern-business-gh-pages/index-level/dateform.php
@@ -1,6 +1,8 @@
 <?php
-  $title="Επικοινωνία - Κλείστε Ραντεβού";
-  require_once "../top.php";
+  // if(!isset($_SESSION)) {
+    // session_start();
+  // }
+
   require_once "../config.php";
   // include_once('sdi1700060.sql');
   // if($_POST['Name']){
@@ -8,9 +10,27 @@
       // if()
   // }
   $res = mysqli_query($link,"Select * FROM regionalunit");
+  $toprint = "";
+  $pesaved = "";
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $pesaved = trim($_POST["pe"]);
+    if($pesaved != "default"){
+        $peres = mysqli_query($link, "SELECT * FROM branch WHERE RegUnit = \"$pesaved\"");
+        $toprint = "<option value=\"default\" selected>Επιλέξτε</option>";
+        while($resrow = mysqli_fetch_array($peres)){
+            $toprint .= "<option value=\"$resrow[0]\">$resrow[0]</option>";
+        }
+    }
+  }
+  
 //path cd /mnt/c/xampp/htdocs
 ?>
 
+<?php
+$title="Επικοινωνία - Κλείστε Ραντεβού";
+require_once "../top.php";
+?>
 <!-- Page Content -->
 <div class="container my-4">
   <!-- NOTE: breadcrumb section starts here -->
@@ -35,20 +55,25 @@
 
 
   <!-- NOTE: contact info section starts here -->
-  <form id="suspForm" action="">
+  <form id="suspForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <h1>Κλείσιμο Ραντεβού</h1>
           <h2 class="py-2">Βρείτε παράρτημα με βάση την περιφερειακή σας ενότητα</h2>
           <div class="form-group row">
             <label for="DOY" class="col-sm-2 col-form-label">Περιφερειακή Ενότητα:</label>
             
-            <select class="select" id="DOY" oninput="showTab()">
-              <option value="default" selected>Επιλέξτε</option>
-             
+            <select class="select" name="pe" id="DOY"> <!--oninput="showTab()">-->
               <?php
-                // $res = mysqli_query($link,"Select * FROM regionalunit");
-                $i = 1;
+                echo "<option value=\"default\"";
+                if(empty($pesaved) || $pesaved =="default"){
+                  echo " selected";
+                }
+                echo ">Επιλέξτε</option>";
                 while( $row = mysqli_fetch_array($res)){
-                    echo "<option value=\"$row[0]\">$row[0]</option>";
+                  echo "<option value=\"$row[0]\"";
+                  if($pesaved == $row[0]){
+                    echo " selected";
+                  }
+                  echo ">$row[0]</option>";
                 }
                ?>
             </select>
@@ -56,39 +81,22 @@
             <div class="invalid-feedback">Το πεδίο είναι υποχρεωτικό.</div>
           </div>
           <div class="form-group row">
-            <div class = "tab">
+            <div class = "tab" style="<?php echo (!empty($toprint)) ? "display: inline;" : "display: none;";?>">
                 <label for="BRANCH" class="col-sm-2 col-form-label">Παράρτημα:</label>
                 <!--Branch drop down-->
             
-                <select class="select" id="BRANCH">
-                 <option value="default" selected>Επιλέξτε</option>
+                <select class="select form-control" id="BRANCH">
                   <?php
-                    
-                    // echo "<script>console.log($i)</script> ";
-                    
-                    $restemp = mysqli_query($link, "SELECT * FROM branch");
-                    
-                    
-                    // $res2 = mysqli_query($link,"Select * FROM branch WHERE RegUnit = ".regunit);
-                    while( $row2 = mysqli_fetch_array($restemp)){
-                        echo "<option value=\"$row2[0]\">$row2[0] - $row2[1]</option>";
+                    if(!empty($toprint)){
+                        echo $toprint;
                     }
                   ?> 
                 </select>
             </div>
-            <?php                    
-                // $res2 = mysqli_query($link,"Select * FROM branch");
-                // while( $row = mysqli_fetch_array($res2)){
-                    // echo "$row[0].$row[1]<br/>";
-                    // // $res2 = mysqli_query($link,"Select * FROM branch WHERE row[0].row[1] == ")
-                // }
-            ?>
-            
-            
             <div class="invalid-feedback">Το πεδίο είναι υποχρεωτικό.</div>
           </div>
 
-        <div class="tab">
+        <!-- <div class="tab">
           <h2>Στοιχεία Υπαλλήλου</h2>
             <div class="form-group row">
               <label for="employeeFirstName" class="col-sm-2 col-form-label">Όνομα:</label>
@@ -118,10 +126,9 @@
                 <div class="invalid-feedback">Το πεδίο είναι υποχρεωτικό.</div>
               </div>
             </div>
-        </div>
+        </div> -->
 
-
-        <button type="submit" class="btn btn-primary" id="nextBtn">Οριστικοποίηση Ραντεβού</button>
+        <button type="submit" class="btn btn-primary" id="nextBtn"><?php echo (empty($pesaved) || $pesaved == "default") ? "Εύρεση παραρτημάτων" : "Οριστικοποίηση Ραντεβού";?></button>
         <!-- Circles which indicates the steps of the form: -->
         <!-- <div style="text-align:center;margin-top:40px;"> -->
           <!-- <span class="step"></span> -->
@@ -146,20 +153,6 @@
       }else{
         x[0].style.display = "none";
       }
-      // <?php
-        // $tempstring = "<option value=\"default\" selected>Επιλέξτε</option>";
-        // $res2 = mysqli_query($link,"Select * FROM branch WHERE RegUnit = ".document.getElementById("DOY").value);
-        // while( $row2 = mysqli_fetch_array($res2)){
-            // $branchstring += "<option value=\"$row2[0]\">$row2[0] - $row2[1]</option>";
-        // }
-      // ?>
-      
-      
-      // if (n == (x.length - 1)) {
-      //   document.getElementById("nextBtn").innerHTML = "Υποβολή";
-      // } else {
-      //   document.getElementById("nextBtn").innerHTML = "Επόμενο";
-      // }
     }
     </script>
 
