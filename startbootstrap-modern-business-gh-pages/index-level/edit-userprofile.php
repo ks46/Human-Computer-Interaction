baeda395c3d146c8577cb768701e9e55b028039d
GKouv89 <gina101@windowslive.com>
2021-01-15 18:28:35 +0200

    Employee has ability to change hasChildYoungerThan12

--- startbootstrap-modern-business-gh-pages/index-level/edit-userprofile.php
+++ startbootstrap-modern-business-gh-pages/index-level/edit-userprofile.php
@@ -3,20 +3,34 @@
     session_start();
   }
   require_once "../config.php";
-  
-  $myCompany = mysqli_query($link, "SELECT Company_Name FROM employer WHERE AFM = ".$_SESSION["AFM"]);
-  $myCompany = mysqli_fetch_array($myCompany)[0];
+
+  $fetchAccountType = "SELECT type FROM user WHERE username = \"".$_SESSION["username"]."\"";
+  $accountType = mysqli_query($link, $fetchAccountType);
+  if(mysqli_num_rows($accountType) == 0){
+    echo "<p>Sth went terribly wrong</p>";
+  }else{
+    $accountTypeRow = mysqli_fetch_array($accountType);  
+    if($accountTypeRow[0] == "employer"){
+      $myCompany = mysqli_query($link, "SELECT Company_Name FROM employer WHERE AFM = ".$_SESSION["AFM"]);
+      $myCompany = mysqli_fetch_array($myCompany)[0];
+    }else{
+      
+    }
+  }
+
   
   if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $doy = trim($_POST["_doy"]);
-
-    
-    $updateDoy = "UPDATE company SET Doy = \"$doy\" WHERE Company_Name= \"$myCompany\"";
-    mysqli_query($link, $updateDoy);
-
-    $updateCompanyName = "UPDATE employer SET Company_Name = \"".$_POST["_companyName"]."\" WHERE AFM = ".$_SESSION["AFM"];
-    mysqli_query($link, $updateCompanyName);
-  
+    if($accountTypeRow[0] == "employer"){
+      $doy = trim($_POST["_doy"]);
+      $updateDoy = "UPDATE company SET Doy = \"$doy\" WHERE Company_Name= \"$myCompany\"";
+      mysqli_query($link, $updateDoy);
+      $updateCompanyName = "UPDATE employer SET Company_Name = \"".$_POST["_companyName"]."\" WHERE AFM = ".$_SESSION["AFM"];
+      mysqli_query($link, $updateCompanyName);
+    }else{
+      $updateChild = "UPDATE employee SET hasChildYoungerThan12 = ".$_POST["_Kid"]." WHERE AFM = ".$_SESSION["AFM"];
+      mysqli_query($link, $updateChild);
+      echo mysqli_error($link);
+    }
     header("location: confirmation.php");
   }
 ?>
@@ -39,58 +53,70 @@
     <!-- Sidebar Column -->
     <h1>Επεξεργασία Στοιχείων</h1>
     <form id="changeInfo" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-      <div class="tab">
-        <p>Οι τιμές στα πεδία είναι αυτές που έχετε μέχρι στιγμής καταχωρήσει στο σύστημα.</br>
-        Αν δεν επιθυμείτε να αλλάξατε κάποιο από τα στοιχεία σας, απλά αγνοήστε το.</p>
         <?php 
-            $fetchAccountType = "SELECT type FROM user WHERE username = \"".$_SESSION["username"]."\"";
-            $accountType = mysqli_query($link, $fetchAccountType);
-            if(mysqli_num_rows($accountType) == 0){
-              echo "<p>Sth went terribly wrong</p>";
-            }else{
-              $accountTypeRow = mysqli_fetch_array($accountType);
-              if($accountTypeRow[0] == "employer"){
-                echo "<div class=\"form-group row\">";
-                echo "<label for=\"companyName\" class=\"col-sm-2 col-form-label\">Νέα Επωνυμία Επιχείρησης</label>";
-                echo "<div class=\"col-10\">";
-                echo "<input type=\"text\" class=\"form-control\" id=\"companyName\" name=\"_companyName\" value=\"$myCompany\" onblur=\"changeNewCompanyName()\">";
-                echo "</div>";
-                echo "</div>";
-                $doy_list = mysqli_query($link, "SELECT * FROM doy ORDER BY Name ASC");
-                $myCompanysDoy = mysqli_query($link, "SELECT Doy FROM company WHERE Company_Name = \"$myCompany\"");
-                $myCompanysDoy = mysqli_fetch_array($myCompanysDoy)[0];
-                echo "<div class=\"form-group row\">";
-                echo "<label for=\"DOY\" class=\"col-sm-2 col-form-label\">Νέα ΔΟΥ Επιχείρησης:</label>";
-                echo "<div class=\"col-10\">";
-                echo "<select class=\"select\" name=\"_doy\" id=\"DOY\" oninput=\"changeNewDoy()\">";
-                while($row = mysqli_fetch_array($doy_list)){
-                  echo "<option value=\"$row[0]\"";
-                  if($myCompanysDoy == $row[0]){
-                    echo " selected";
-                  }
-                  echo ">$row[0]</option>";
-                }
-                echo "</select>";
-                echo "</div>";
-                echo "</div>";
+          if($accountTypeRow[0] == "employer"){
+            echo "<div class=\"tab\">";      
+            echo "<p>Οι τιμές στα πεδία είναι αυτές που έχετε μέχρι στιγμής καταχωρήσει στο σύστημα.</br>
+            Αν δεν επιθυμείτε να αλλάξατε κάποιο από τα στοιχεία σας, απλά αγνοήστε το.</p>";
+            echo "<div class=\"form-group row\">";
+            echo "<label for=\"companyName\" class=\"col-sm-2 col-form-label\">Νέα Επωνυμία Επιχείρησης</label>";
+            echo "<div class=\"col-10\">";
+            echo "<input type=\"text\" class=\"form-control\" id=\"companyName\" name=\"_companyName\" value=\"$myCompany\" onblur=\"changeNewCompanyName()\">";
+            echo "</div>";
+            echo "</div>";
+            $doy_list = mysqli_query($link, "SELECT * FROM doy ORDER BY Name ASC");
+            $myCompanysDoy = mysqli_query($link, "SELECT Doy FROM company WHERE Company_Name = \"$myCompany\"");
+            $myCompanysDoy = mysqli_fetch_array($myCompanysDoy)[0];
+            echo "<div class=\"form-group row\">";
+            echo "<label for=\"DOY\" class=\"col-sm-2 col-form-label\">Νέα ΔΟΥ Επιχείρησης:</label>";
+            echo "<div class=\"col-10\">";
+            echo "<select class=\"select\" name=\"_doy\" id=\"DOY\" oninput=\"changeNewDoy()\">";
+            while($row = mysqli_fetch_array($doy_list)){
+              echo "<option value=\"$row[0]\"";
+              if($myCompanysDoy == $row[0]){
+                echo " selected";
               }
+              echo ">$row[0]</option>";
             }
+            echo "</select>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "<div class=\"tab\">";
+            echo "<h2>Νέα στοιχεία Επιχείρησης</h2>";
+            echo "<p id=\"newCompanyName\">Νέα Επωνυμία Επιχείρησης: <b><?php echo $myCompany; ?></b></p>"; 
+            echo "<p id=\"newDoy\">Νέα ΔΟΥ: <b><?php echo $myCompanysDoy; ?></b></p>"; 
+            echo "<p>Αν είστε σίγουροι για τα νέα στοιχεία, πατήστε 'Υποβολή', αλλιώς πατήστε 'Προηγούμενο' για να τα τροποποιήσετε περαιτέρω.</p>";
+            echo "</div>";
+          }else{
+            echo "<div class=\"tab\">";
+            $hasChildUnder12 = "SELECT hasChildYoungerThan12 FROM employee WHERE AFM = ".$_SESSION["AFM"];
+            $hasChildUnder12 = mysqli_query($link, $hasChildUnder12);
+            $hasChildUnder12 = mysqli_fetch_array($hasChildUnder12)[0];
+            if($hasChildUnder12 == 1){
+              echo "<p>Αν πλέον δεν έχετε τέκνο κάτω των 12 ετών, τότε μπορείτε να το δηλώσετε παρακάτω</p>";
+              echo "<div class=\"form-group row\">";
+              echo "<label for=\"noKidYounger12\" class=\"form-check-label\">Δεν έχω πλέον τέκνο κάτω των 12 ετών</label>";
+              echo "<input type=\"checkbox\" class=\"form-check-input\" id=\"noKidYounger12\" name=\"_Kid\" value=\"0\">";
+              echo "</div>";
+            }else{
+              echo "<p>Αν αποκτήσατε πρόσφατα τέκνο, τότε μπορείτε να το δηλώσετε παρακάτω ώστε να γίνετε δικαιούχοι της άδειας ειδικού σκοπού.</p>";
+              echo "<div class=\"form-group row\">";
+              echo "<label for=\"newKidYounger12\" class=\"form-check-label\">Απέκτησα τέκνο κάτω των 12 ετών</label>";
+              echo "<input type=\"checkbox\" class=\"form-check-input\" id=\"newKidYounger12\" name=\"_Kid\" value=\"1\">";
+              echo "</div>";
+            }
+            echo "</div>";
+          }
         ?>
-      </div>
-      <div class="tab">
-        <h2>Νέα στοιχεία Επιχείρησης</h2>
-          <p id="newCompanyName">Νέα Επωνυμία Επιχείρησης: <b><?php echo $myCompany; ?></b></p> 
-          <p id="newDoy">Νέα ΔΟΥ: <b><?php echo $myCompanysDoy; ?></b></p> 
-          <p>Αν είστε σίγουροι για τα νέα στοιχεία, πατήστε 'Υποβολή', αλλιώς πατήστε 'Προηγούμενο' για να τα τροποποιήσετε περαιτέρω.</p>
-      </div>
-      <div style="overflow:auto;">
-        <div style="float:right;">
-          <button type="button" class = "btn btn-primary" id="prevBtn" onclick="nextPrev(-1)">Προηγούμενο</button>
-          <button type="button" class = "btn btn-primary" id="nextBtn" onclick="nextPrev(1)">Επόμενο</button>        
+        <div style="overflow:auto;">
+          <div style="float:right;">
+            <button type="button" class = "btn btn-primary" id="prevBtn" onclick="nextPrev(-1)">Προηγούμενο</button>
+            <button type="button" class = "btn btn-primary" id="nextBtn" onclick="nextPrev(1)">Επόμενο</button>        
+          </div>
         </div>
       </div>
    </form>    
-  </div>
   
   <script>
     var currentTab = 0; // Current tab is set to be the first tab (0)
