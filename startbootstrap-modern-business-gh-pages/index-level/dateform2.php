386073847bde91eb41130ef11e06128128f92938
GKouv89 <gina101@windowslive.com>
2021-01-18 16:14:23 +0200

    Fully booked days not available in datepicker. appdatepicker file is not useful anymore. Some stuff necessary for jquery is now included in top.php

--- startbootstrap-modern-business-gh-pages/index-level/dateform2.php
+++ startbootstrap-modern-business-gh-pages/index-level/dateform2.php
@@ -22,8 +22,13 @@
           if(mysqli_stmt_execute($apptStmt)){
             mysqli_stmt_store_result($apptStmt);
             if(mysqli_stmt_num_rows($apptStmt) == 6){
-              if(!in_array($date, $fullyBookedDates)){
-                array_push($fullyBookedDates, $date);
+              $year = str_split($date, 4)[0];
+              $month = str_split(str_split($date, 5)[1], 2)[0];
+              $day = str_split(str_split($date, 8)[1], 2)[0];
+              $aDate = array($day, $month, $year);
+                
+              if(!in_array($aDate, $fullyBookedDates)){
+                array_push($fullyBookedDates, $aDate);
               }
             }
           }else{
@@ -41,30 +46,10 @@
   }else{
     echo "<h1>".mysqli_stmt_error($stmt)."</h1>";
   }
-  
   $title="COVID-19 - Άδεια Ειδικού Σκοπού - Πληροφορίες";
-  // require_once "../top.php";
+  require_once "../top.php";
 ?>
 
-<html lang="el">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- <meta name="description" content=""> -->
-  <!-- <meta name="author" content=""> -->
-
-  <title><?php echo $title ?> - Υπουργείο Εργασίας</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-  <link href="../css/styles.css" rel="stylesheet">
-  <link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css"
-         rel = "stylesheet">
-  
-</head>
-
-<body>
   <div class="container my-4">
     <!-- NOTE: breadcrumb section starts here -->
     <nav aria-label="breadcrumb">
@@ -105,118 +90,153 @@
             
             <!---->
             
-            <input type="time" id="TimeSusp">
+            <!-- <input type="time" id="TimeSusp"> -->
             <div class="invalid-feedback">Η επιλογή ημερομηνίας είναι υποχρεωτική.</div>
         </div>
     </div>
   </div>
           
   <script>
-    function valiDate(){
-        var br = document.getElementById("BRANCH");
-        var fn = document.getElementById("FirstName");
-        var ln = document.getElementById("LastName");
-        var valid1 = true;
-        var valid2 = true;
-        var valid3 = true;
-        // var valid = true;
-        var erstr1 = new String();
-        var erstr2 = new String();
-        var erstr3 = new String();
-        
-        var frow1 = br.parentNode;
-        var frow2 = fn.parentNode.parentNode;
-        var frow3 = ln.parentNode.parentNode;
-        
-        var ncn1;
-        var ncn2;
-        var ncn3;
-        
-        //var x, i, input, valid = true;
-        // x = document.getElementsByClassName("tab");
-        // formGroups = x[currentTab].getElementsByClassName("form-group row");
-        
-        // for(i=0;i<formGroups.length;i++{
-            // input = formGroups[i].getElementsByTagName("input");
-            if(br.value == "default"){
-                if(br.className.indexOf(" is-invalid") == -1){
-                    br.className += " is-invalid";
-                }
-                erstr1+= "Η επιλογή παραρτήματος είναι υποχρεωτική!\n";
-                valid1 = false;
-            }
-            if(fn == ""){ 
-                if(fn.className.indexOf(" is-invalid") == -1){
-                    fn.className += " is-invalid";
-                }
-                erstr2+= "Η εισαγωγή ονόματος είναι υποχρεωτική!\n";
-                valid2 = false;
-            }
-            if(ln.value == ""){
-                if(ln.className.indexOf(" is-invalid") == -1){
-                    ln.className += " is-invalid";
-                }
-                erstr3+= "Η εισαγωγή επωνύμου είναι υποχρεωτική!\n";
-                valid3 = false;
-            }
-            
-            if(!valid1){
-                if(br.className.indexOf(" is-invalid") == -1){
-                    br.className += " is-invalid";
-                }
-                if(frow1.className.indexOf(" has-danger") == -1){
-                    frow1.className += " has-danger";
-                }
-            }else{
-                if(br.className.indexOf(" is-invalid") !== -1){
-                    ncn1 = br.className.replace(" is-invalid", "");
-                    br.className = ncn1;
-                }
-                if(frow1.className.indexOf(" has-danger") !== -1){
-                    ncn1 = frow1.className.replace(" has-danger", "");
-                }
-                br.nextSibling.innerHTML = "Το πεδίο είναι υποχρεωτικό.";
-            }
-            
-            if(!valid2){
-                if(fn.className.indexOf(" is-invalid") == -1){
-                    fn.className += " is-invalid";
-                }
-                if(frow2.className.indexOf(" has-danger") == -1){
-                    frow2.className += " has-danger";
-                }
-            }else{
-                if(fn.className.indexOf(" is-invalid") !== -1){
-                    ncn2 = fn.className.replace(" is-invalid", "");
-                    fn.className = ncn2;
-                }
-                if(frow2.className.indexOf(" has-danger") !== -1){
-                    ncn2 = frow2.className.replace(" has-danger", "");
-                }
-                fn.nextSibling.nextSibling.innerHTML = "Το πεδίο είναι υποχρεωτικό.";
-            }
-            
-            if(!valid3){
-                if(ln.className.indexOf(" is-invalid") == -1){
-                    ln.className += " is-invalid";
-                }
-                if(frow3.className.indexOf(" has-danger") == -1){
-                    frow3.className += " has-danger";
-                }
-            }else{
-                if(ln.className.indexOf(" is-invalid") !== -1){
-                    ncn3 = ln.className.replace(" is-invalid", "");
-                    ln.className = ncn3;
-                }
-                if(frow3.className.indexOf(" has-danger") !== -1){
-                    ncn3 = frow3.className.replace(" has-danger", "");
-                }
-                ln.nextSibling.nextSibling.innerHTML = "Το πεδίο είναι υποχρεωτικό.";
-            }
-        // }
-        return valid;
+    <?php echo "var invalidDates = [";
+    for($i = 0; $i < count($fullyBookedDates); $i++){
+      echo "[".$fullyBookedDates[$i][0].", ".$fullyBookedDates[$i][1].", ".$fullyBookedDates[$i][2]."]";
+      if($i != count($fullyBookedDates) - 1){
+        echo ", ";
+      }
     }
+    echo "];";
+    ?>
     
+    var toTest = 3;
+    $(document).ready(function(){
+      $("#begOfSusp").datepicker({beforeShowDay:nonworkingdates});
+      function nonworkingdates(datep){
+          var day = datep.getDay();
+          if(day == 0 || day == 1 || day == 6){
+              return [false];
+          }
+          var date = datep.getDate();
+          var month = datep.getMonth() + 1;
+
+          for (var i = 0; i < invalidDates.length; i++) {
+            if(datep.getFullYear() == invalidDates[i][2]){
+              if(month.toString() == invalidDates[i][1].toString()){
+                if(date.toString() == invalidDates[i][0].toString()){
+                  return [false];
+                }
+              }
+            }
+          }
+          return [true];
+      }
+    });
+
+
+
+   // function valiDate(){
+        // var br = document.getElementById("BRANCH");
+        // var fn = document.getElementById("FirstName");
+        // var ln = document.getElementById("LastName");
+        // var valid1 = true;
+        // var valid2 = true;
+        // var valid3 = true;
+        // // var valid = true;
+        // var erstr1 = new String();
+        // var erstr2 = new String();
+        // var erstr3 = new String();
+        
+        // var frow1 = br.parentNode;
+        // var frow2 = fn.parentNode.parentNode;
+        // var frow3 = ln.parentNode.parentNode;
+        
+        // var ncn1;
+        // var ncn2;
+        // var ncn3;
+        
+        // //var x, i, input, valid = true;
+        // // x = document.getElementsByClassName("tab");
+        // // formGroups = x[currentTab].getElementsByClassName("form-group row");
+        
+        // // for(i=0;i<formGroups.length;i++{
+            // // input = formGroups[i].getElementsByTagName("input");
+            // if(br.value == "default"){
+                // if(br.className.indexOf(" is-invalid") == -1){
+                    // br.className += " is-invalid";
+                // }
+                // erstr1+= "Η επιλογή παραρτήματος είναι υποχρεωτική!\n";
+                // valid1 = false;
+            // }
+            // if(fn == ""){ 
+                // if(fn.className.indexOf(" is-invalid") == -1){
+                    // fn.className += " is-invalid";
+                // }
+                // erstr2+= "Η εισαγωγή ονόματος είναι υποχρεωτική!\n";
+                // valid2 = false;
+            // }
+            // if(ln.value == ""){
+                // if(ln.className.indexOf(" is-invalid") == -1){
+                    // ln.className += " is-invalid";
+                // }
+                // erstr3+= "Η εισαγωγή επωνύμου είναι υποχρεωτική!\n";
+                // valid3 = false;
+            // }
+            
+            // if(!valid1){
+                // if(br.className.indexOf(" is-invalid") == -1){
+                    // br.className += " is-invalid";
+                // }
+                // if(frow1.className.indexOf(" has-danger") == -1){
+                    // frow1.className += " has-danger";
+                // }
+            // }else{
+                // if(br.className.indexOf(" is-invalid") !== -1){
+                    // ncn1 = br.className.replace(" is-invalid", "");
+                    // br.className = ncn1;
+                // }
+                // if(frow1.className.indexOf(" has-danger") !== -1){
+                    // ncn1 = frow1.className.replace(" has-danger", "");
+                // }
+                // br.nextSibling.innerHTML = "Το πεδίο είναι υποχρεωτικό.";
+            // }
+            
+            // if(!valid2){
+                // if(fn.className.indexOf(" is-invalid") == -1){
+                    // fn.className += " is-invalid";
+                // }
+                // if(frow2.className.indexOf(" has-danger") == -1){
+                    // frow2.className += " has-danger";
+                // }
+            // }else{
+                // if(fn.className.indexOf(" is-invalid") !== -1){
+                    // ncn2 = fn.className.replace(" is-invalid", "");
+                    // fn.className = ncn2;
+                // }
+                // if(frow2.className.indexOf(" has-danger") !== -1){
+                    // ncn2 = frow2.className.replace(" has-danger", "");
+                // }
+                // fn.nextSibling.nextSibling.innerHTML = "Το πεδίο είναι υποχρεωτικό.";
+            // }
+            
+            // if(!valid3){
+                // if(ln.className.indexOf(" is-invalid") == -1){
+                    // ln.className += " is-invalid";
+                // }
+                // if(frow3.className.indexOf(" has-danger") == -1){
+                    // frow3.className += " has-danger";
+                // }
+            // }else{
+                // if(ln.className.indexOf(" is-invalid") !== -1){
+                    // ncn3 = ln.className.replace(" is-invalid", "");
+                    // ln.className = ncn3;
+                // }
+                // if(frow3.className.indexOf(" has-danger") !== -1){
+                    // ncn3 = frow3.className.replace(" has-danger", "");
+                // }
+                // ln.nextSibling.nextSibling.innerHTML = "Το πεδίο είναι υποχρεωτικό.";
+            // }
+        // // }
+        // return valid;
+    // }     
   </script>
 
   <!-- NOTE: Footer section starts here -->
@@ -225,15 +245,6 @@
       <p class="m-0 text-center">Copyright &copy; Ypakp 2020</p>
     </div>
   </footer>
-  <!-- NOTE: Footer section ends here -->
-  <!-- Contact form JavaScript -->
-  <!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-  <!-- Bootstrap core JavaScript -->
-      
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script src="../js/appdatepick.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>      
